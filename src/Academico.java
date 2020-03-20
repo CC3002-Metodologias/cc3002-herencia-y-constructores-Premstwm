694606aa19b1cd652e916bf69941eadeed4ea541
Dante <dante.carcamo@ug.uchile.cl>
2020-03-20 13:56:01 -0300

    solucion aux1

--- src/Academico.java
+++ src/Academico.java
@@ -1,2 +1,22 @@
-public class Academico {
+public class Academico extends Persona{
+    private String paper;
+
+    public Academico( String nombre, String ocupacion, int edad, String paper){
+        super(nombre, ocupacion, edad);
+        this.paper = paper;
+    }
+
+    public void comer() {
+        super.comer();
+    }
+
+    public void dormir() {
+        System.out.println("ZzzZzzZ");
+    }
+
+    public String hacerLoSuyo() {
+        return "Aprender a usar Zoom";
+    }
+
+    public void publicarPaper(){ System.out.println(this.paper);}
 }
--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,2 +1,16 @@
-public class Estudiante {
+public class Estudiante extends Persona {
+    public Estudiante( String nombre, String ocupacion, int edad){
+        super(nombre, ocupacion, edad);
+    }
+    public void comer() {
+        System.out.println("Ñom Ñom Gorbea");
+    }
+
+    public void dormir() {
+        System.out.println("ZzzZzz");
+    }
+
+    public String hacerLoSuyo() {
+        return "Ver animé y series en Netflix";
+    }
 }
